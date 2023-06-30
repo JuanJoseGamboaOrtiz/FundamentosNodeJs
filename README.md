f41f002bf7fd348c4241714a617cc2b77726d10e
Juan José Gamboa Ortiz <juangamboaortiz@gmail.com>
2023-06-30 16:19:48 -0500

    docs: :memo: Introducción a Node y Caracteristicas
    Se Ingresa información de lo que es NojdeJS y las caracteristicas de Node

---
+++ README.md
@@ -0,0 +1,5 @@
+# Repositorio de Fundamentos de NodeJS
+
+¡Bienvenido(a) al repositorio de Fundamentos Básicos de PHP y Taller de Arrays! Aquí encontrarás ejemplos y problemas resueltos utilizando métodos de array en PHP.
+
+
---
+++ app.js
@@ -0,0 +1,64 @@
+/**
+ * ! NodeJS Basico
+ * 
+ * !1.1 Introducción a  Node
+ * 
+ *  Node.js es un entorno de ejecución de código JavaScript que se basa en el motor
+    de JavaScript V8 de Google Chrome. A diferencia de la ejecución de JavaScript en
+    un navegador web, Node.js permite ejecutar código JavaScript en el lado del
+    servidor. Esto significa que puedes usar JavaScript para crear aplicaciones de
+    servidor y realizar tareas del lado del servidor, como manipular archivos,
+    interactuar con bases de datos y responder a solicitudes de clientes.
+    Node.js utiliza un modelo de E/S sin bloqueo y basado en eventos, lo que significa
+    que puede manejar muchas solicitudes simultáneamente sin bloquear el flujo de
+    ejecución. Esto lo hace ideal para aplicaciones de alto rendimiento y en tiempo
+    real, como aplicaciones web, servicios de backend, APIs y microservicios.
+    Además de su capacidad para ejecutar código JavaScript en el servidor, Node.js
+    también cuenta con un administrador de paquetes llamado npm (Node Package
+    Manager). npm te permite descargar, instalar y administrar fácilmente paquetes y
+    módulos de JavaScript de código abierto que puedes utilizar en tus aplicaciones.
+    En resumen, Node.js es una plataforma que permite ejecutar código JavaScript en
+    el servidor, lo cual ha impulsado el desarrollo de aplicaciones web y servicios del
+    lado del servidor de alto rendimiento y escalables.
+
+ * !1.2 Características de Node
+
+ * ?  1.2.1   JavaScript en el Servidor
+    Node.js permite ejecutar código JavaScript en el servidor, lo que facilita el uso del
+    mismo lenguaje en el frontend y el backend. Esto permite a los desarrolladores
+    utilizar las mismas habilidades y compartir código entre la parte del cliente y la del
+    servidor.
+ * ? 1.2.2 Modelo de E/S sin bloqueo y basado en eventos
+    Node.js utiliza un modelo de E/S sin bloqueo y basado en eventos, lo que significa que puede manejar
+    múltiples solicitudes concurrentemente sin bloquear el flujo de ejecución. Esto lo
+    hace muy eficiente en términos de rendimiento y escalabilidad, lo que resulta en
+    aplicaciones rápidas y con capacidad de respuesta.
+ * ? 1.2.3 Arquitectura Orientada a módulos
+    Node.js utiliza el sistema de módulos de
+    CommonJS, lo que facilita la reutilización de código y la creación de aplicaciones
+    modularizadas. Esto permite a los desarrolladores separar el código en módulos yutilizar paquetes de terceros fácilmente a través del administrador de paquetes
+    npm.
+ * ? 1.2.4 Amplia comunidad y ecosistema 
+    Node.js cuenta con una gran comunidad de
+    desarrolladores y un ecosistema vibrante. Hay una amplia gama de bibliotecas,
+    frameworks y herramientas disponibles en el registro de paquetes npm, lo que
+    facilita el desarrollo rápido de aplicaciones y la integración con otros sistemas.
+
+ * ? 1.2.5 Escabilidad
+    Node.js es conocido por su capacidad para manejar grandes
+    cantidades de solicitudes concurrentes con eficiencia. Gracias a su modelo sin
+    bloqueo y su enfoque en la escalabilidad, es ampliamente utilizado para
+    desarrollar aplicaciones en tiempo real, como chats en línea, juegos multijugador y
+    sistemas de streaming.
+ * ? 1.2.6 Orientado a Eventos
+    Node.js se basa en un patrón de programación
+    orientado a eventos, lo que significa que las operaciones se desencadenan en
+    respuesta a eventos específicos. Esto hace que Node.js sea adecuado para el
+    desarrollo de aplicaciones en tiempo real y de transmisión de datos, donde se
+    requiere una actualización constante y una respuesta rápida a los eventos.
+    Estas son solo algunas de las características clave de Node.js que han contribuido
+    a su popularidad y adopción en el desarrollo web y de aplicaciones del lado del servidor.
+
+ * ! 1.3 Como funciona Node
+
+ */
