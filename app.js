105d186b0b84a26dfb31bfe457c3cfb161e8b2ad
Juan José Gamboa Ortiz <juangamboaortiz@gmail.com>
2023-06-30 16:28:47 -0500

    docs: :memo: Como funciona NOde
    Se explica como funciona node y un paso a paso

--- app.js
+++ app.js
@@ -60,5 +60,53 @@
     a su popularidad y adopción en el desarrollo web y de aplicaciones del lado del servidor.
 
  * ! 1.3 Como funciona Node
+    Node.js se basa en una arquitectura de tiempo de ejecución de JavaScript basada
+    en eventos y utiliza un solo subproceso (thread) principal para ejecutar el código
+    JavaScript. Aquí está el proceso general de cómo funciona Node.js:
+
+ * ? 1.3.1 Inicialización 
+    Cuando se inicia una aplicación Node.js, se realiza una
+    inicialización del entorno de ejecución. Se cargan los módulos necesarios, se
+    establecen las variables de entorno y se configuran los parámetros de ejecución
+ * ? 1.3.2 Event Loop y manejo de eventos
+    Node.js utiliza un bucle de eventos (event
+    loop) para manejar las solicitudes y las operaciones de E/S de manera
+    asincrónica. El bucle de eventos es responsable de recibir y distribuir eventos en
+    el programa. Node.js espera continuamente eventos y ejecuta las devoluciones de
+    llamada asociadas a medida que ocurren.
+ * ? 1.3.3 Gestión de solicitud
+    Cuando llega una solicitud al servidor Node.js, se
+    desencadena un evento. El servidor registra una devolución de llamada (callback)
+    para manejar la solicitud y continúa con otras tareas mientras la solicitud se
+    procesa en segundo plano.
+ * ? 1.3.4 Operaciones de E/S no bloqueantes
+    Node.js utiliza operaciones de E/S no
+    bloqueantes, lo que significa que puede iniciar una operación de E/S (como leer oescribir en un archivo, hacer una solicitud a una base de datos o enviar una
+    solicitud HTTP) y continuar con otras tareas sin esperar a que se complete la
+    operación. Una vez que la operación de E/S se completa, se invoca la devolución
+    de llamada correspondiente y se maneja la respuesta.
+ * ? 1.3.5 Devoluciones de llamada (callbacks)
+    Las devoluciones de llamada son
+    funciones que se registran para ejecutarse después de que se complete una
+    operación asincrónica. Las devoluciones de llamada se ejecutan cuando se
+    produce un evento o cuando se completa una operación de E/S. Esto permite que
+    Node.js maneje múltiples solicitudes y operaciones de manera eficiente y no
+    bloqueante.
+ * ? 1.3.6 Módulos y Npm
+    Node.js utiliza un sistema de módulos basado en
+    CommonJS, lo que permite la modularidad y la reutilización de código. Los
+    módulos se cargan bajo demanda y se pueden utilizar para organizar el código en
+    diferentes archivos y directorios. npm (Node Package Manager) es un
+    administrador de paquetes utilizado en Node.js que facilita la instalación,
+    actualización y administración de bibliotecas y módulos de terceros.
+
+    En resumen, Node.js funciona mediante el uso de un bucle de eventos,
+    operaciones de E/S no bloqueantes y devoluciones de llamada. Esto permite que
+    Node.js maneje múltiples solicitudes y operaciones de manera eficiente, lo que lo
+    hace adecuado para aplicaciones en tiempo real y de alta concurrencia.
+    
+
+
+
 
  */
