822550f5889fec5b92abeafe1af63bae655aa12f
Juan José Gamboa Ortiz <juangamboaortiz@gmail.com>
2023-07-06 14:49:03 -0500

    lo que sea

--- app.js
+++ app.js
@@ -195,7 +195,255 @@
             
         Siguiendo estos pasos, podrás ejecutar el archivo "main.js" en Node.js desde la
         terminal de Visual Studio Code.
+ *  ! 1.6 Diferencias de Apis Globlales JavaScript en Node
         
+        * !1.6.1. global y window
+
+        En JavaScript, la palabra clave "window" se utiliza en el entorno de un navegador
+        ventana del navegador y contiene propiedades y métodos relacionados con ella,
+        web para hacer referencia al objeto global que representa la ventana delnavegador. 
+        El objeto "window" proporciona una interfaz para interactuar con la
+        como manipulación del DOM, gestión de eventos, manipulación de cookies,
+        control de ventanas emergentes, entre otros. En resumen, "window" en JavaScript
+        se refiere al objeto global que representa la ventana del navegador en el entorno
+        de un navegador web.
+        Por otro lado, en Node.js, no existe un objeto "window" como en el entorno del
+        navegador. En su lugar, Node.js utiliza el objeto "global" como su objeto global. El
+        objeto "global" en Node.js proporciona un ámbito global para variables, funciones y
+        objetos en una aplicación de Node.js. A través del objeto "global", se pueden
+        definir variables y funciones que estarán disponibles en todos los módulos y
+        archivos de la aplicación. A diferencia del objeto "window" en el navegador, el
+        objeto "global" en Node.js no está relacionado con la ventana del navegador y
+        ofrece un conjunto diferente de propiedades y métodos, adecuados para el
+        entorno de ejecución de Node.js.
+        En resumen, en el entorno del navegador, "window" se refiere al objeto global que
+        representa la ventana del navegador, mientras que en Node.js, "global" se refiere
+        al objeto global en el entorno de ejecución de Node.js, proporcionando un ámbito
+        global para variables y funciones en la aplicación.
+
+        * !1.6.2 process
+
+        En Node.js, el objeto process es un objeto global que proporciona información y
+        control sobre el proceso actual en ejecución. Es una instancia de la clase
+        EventEmitter, lo que significa que puede emitir y escuchar eventos.
+
+        process.exit([código]): Un método que finaliza el proceso actual. Puedes
+        proporcionar un código de salida opcional como argumento para indicar el estado
+        de salida del proceso.
+
+        process.env: Un objeto que contiene las variables de entorno del sistema.
+        Puedes acceder a estas variables para obtener información como la configuración
+        del sistema, rutas de archivo, claves de API, etc.
+
+        process.stdin, process.stdout y process.stderr: Estos objetos permiten la
+        interacción con la entrada estándar (stdin), la salida estándar (stdout) y la salida
+        de error estándar (stderr) respectivamente. Puedes leer datos de la entrada
+        estándar, escribir en la salida estándar y mostrar mensajes de error en la salida de
+        error estándar.
+
+        process.on(evento, callback): Un método para registrar devoluciones de llamada
+        para manejar eventos del proceso. Puedes utilizar este método para capturar
+        eventos como la salida del proceso, errores no capturados, señales del sistema
+        operativo, etc.
+
+    * !1.7 Sistema de módulos
+        El sistema de módulos de Node.js es un componente fundamental de la
+        plataforma que permite la modularidad y reutilización de código en aplicaciones yproyectos desarrollados con Node.js. 
+        Proporciona una forma organizada y
+        estructurada de organizar y distribuir el código en diferentes archivos y directorios.
+        El sistema de módulos de Node.js se basa en el concepto de módulos, que son
+        unidades de código independientes que encapsulan funcionalidades específicas.
+        Cada módulo tiene su propio ámbito y puede exportar funciones, objetos o
+        variables para ser utilizados por otros módulos.
+        *!1.7.1 Common JS
+
+        CommonJS es una especificación de módulos para JavaScript que define un
+        formato estándar para la organización y carga de módulos en entornos de
+        ejecución, como Node.js. Fue diseñado con el objetivo de proporcionar una
+        solución para modularizar el código JavaScript y facilitar la reutilización de
+        módulos en diferentes proyectos.
+
+        Las características principales de CommonJS son:
+
+        *? Síncrono:
+
+        El sistema de módulos CommonJS utiliza una carga de módulos
+        síncrona, lo que significa que los módulos se cargan y evalúan de forma
+        secuencial antes de que el programa principal pueda continuar su ejecución. Esto
+        permite que los módulos dependientes estén disponibles de inmediato.
+
+        *?Carga bajo demanda: 
+        Los módulos se cargan solo cuando son requeridos
+        mediante la función require(). Esto permite una carga eficiente de los módulos
+        necesarios y evita la carga innecesaria de módulos que no se utilizan.3
+
+        *?Exportación   de importación de módulos:
+        CommonJS proporciona una forma
+        estándar de exportar e importar funcionalidades entre módulos. Los módulos
+        pueden utilizar el objeto module.exports para exportar variables, objetos o
+        funciones, y la función require() para importar los módulos necesarios.
+        *?Resolución de Rutas:
+        CommonJS proporciona una resolución de rutas relativas
+        en la carga de módulos. Se utiliza el prefijo ./ para referenciar módulos locales y el
+        prefijo ../ para subir un nivel en la jerarquía de directorios.
+        *?Ámbito propio:
+        Cada módulo CommonJS tiene su propio ámbito (scope), lo que
+        significa que las variables y las funciones definidas en un módulo no están
+        disponibles de forma predeterminada en otros módulos. Esto ayuda a evitar
+        conflictos de nombres y proporciona una encapsulación adecuada.
+        *?Módulos incorporados:
+        CommonJS incluye un conjunto de módulos
+        incorporados (Core Modules) que están disponibles de forma predeterminada sin
+        necesidad de instalar ninguna dependencia adicional. Estos módulos incluyenfuncionalidades comunes como fs para operaciones de sistema de archivos, http
+        para la creación de servidores web y path para manejar rutas de archivos y
+        directorios.
+        
+        Es importante tener en cuenta que CommonJS es ampliamente utilizado en
+        entornos de servidor como Node.js, pero no es compatible de forma nativa en los
+        navegadores web. En los navegadores, se utiliza el formato de módulos ES6
+        (ECMAScript 2015) o se requiere el uso de herramientas de construcción (como
+        Webpack o Browserify) para convertir los módulos CommonJS en un formato
+        compatible con el navegador. Si deseas obtener más información sobre las
+        commonJS visita: https://www.tutorialsteacher.com/nodejs/nodejs-module-exports
+        
+        *!1.7.1.1 require JSON
+        CommonJS es posible importar archivos JSON directamente como módulos.
+        Cuando se importa un archivo JSON, CommonJS lo interpreta automáticamente y
+        lo devuelve como un objeto JavaScript.
+
+        En este ejemplo, el archivo datos.json contiene un objeto JSON con diferentes
+        propiedades. Luego, en main.js, se utiliza require() para importar el archivo JSON
+        como un módulo y asignarlo a la variable datos. A partir de ahí, se puede acceder
+        a las propiedades del objeto JSON normalmente.
+        Es importante tener en cuenta que, a diferencia de los módulos JavaScript, los
+        archivos JSON no tienen una funcionalidad dinámica o código ejecutable. Se
+        cargan simplemente como objetos JavaScript estáticos
+        *!1.7.1.1 require fs
+        El módulo fs (File System) en Node.js proporciona una amplia gama de métodos
+        para interactuar con el sistema de archivos del sistema operativo. A continuación,
+        se mencionan algunos de los métodos más utilizados del módulo fs:
+        1. fs.readFile(path, options, callback): Lee el contenido de un archivo de
+        forma asincrónica.
+
+         path: Ruta del archivo a leer.
+         options: Opciones de lectura, como la codificación.
+         callback: Función de devolución de llamada que recibe err (error) y data
+        (contenido del archivo).
+        2. fs.readFileSync(path, options): Lee el contenido de un archivo de forma
+        sincrónica (bloqueante).
+         path: Ruta del archivo a leer.
+         options: Opciones de lectura, como la codificación. Devuelve el contenido
+        del archivo.
+        3. fs.writeFile(file, data, options, callback):Escribe datos en un archivo de
+        forma asincrónica.
+         file: Ruta del archivo en el que se escribirán los datos.
+         data: Datos a escribir en el archivo.
+         options: Opciones de escritura, como la codificación.
+         callback: Función de devolución de llamada que recibe err (error) cuando
+        se completa la escritura.
+        4. fs.writeFileSync(file, data, options):Escribe datos en un archivo de forma
+        sincrónica (bloqueante).
+         file: Ruta del archivo en el que se escribirán los datos.
+         data: Datos a escribir en el archivo.
+         options: Opciones de escritura, como la codificación.5. fs.appendFile(file, data, options, callback): Agrega datos al final de un
+        archivo de forma asincrónica.
+         file: Ruta del archivo al que se agregarán los datos.
+         data: Datos a agregar al archivo.
+         options: Opciones de escritura, como la codificación.
+         callback: Función de devolución de llamada que recibe err (error) cuando
+        se completa la operación.
+        6. fs.appendFileSync(file, data, options): Agrega datos al final de un archivo
+        de forma sincrónica (bloqueante)
+         file: Ruta del archivo al que se agregarán los datos.
+         data: Datos a agregar al archivo.
+         options: Opciones de escritura, como la codificación.
+        7. fs.readdir(path, callback):Lee el contenido de un directorio de forma
+        asincrónica.
+         path: Ruta del directorio a leer.
+         callback: Función de devolución de llamada que recibe err (error) y files
+        (array de nombres de archivos en el directorio).
+        8. fs.readdirSync(path):Lee el contenido de un directorio de forma sincrónica
+        (bloqueante).
+         path: Ruta del directorio a leer. Devuelve un array de nombres de archivos
+        en el directorio.
+        9. fs.mkdir(path, options, callback): Crea un directorio de forma asincrónica.
+         path: Ruta del directorio a crear.
+         options: Opciones para la creación del directorio.
+         callback: Función de devolución de llamada que recibe err (error) cuando
+        se completa la creación.
+        10. fs.mkdirSync(path, options): Crea un directorio de forma sincrónica
+        (bloqueante).
+         path: Ruta del directorio a crear.
+         options: Opciones para la creación del directorio.
+        11. fs.rmdir(path, callback): Elimina un directorio de forma asincrónica.
+         path: Ruta del directorio a eliminar.
+         callback: Función de devolución de llamada que recibe err (error) cuando
+        se completa la eliminación.
+        12. fs.rmdirSync(path):Elimina un directorio de forma sincrónica (bloqueante).
+
+        *!1.7.1.3 Require http
+
+        El módulo https en Node.js proporciona una API para realizar solicitudes HTTP
+        seguras (HTTPS). A continuación, se mencionan algunos de los métodos más
+        utilizados del módulo https:
+
+        1. http.get(options, callback):Realiza una solicitud HTTP GET a través de
+        HTTPS.
+         options: Opciones de la solicitud, como la URL y los encabezados.
+         callback: Función de devolución de llamada que se ejecuta cuando se
+        completa la respuesta. Recibe el objeto de respuesta.
+        2. http.request(options, callback):Inicia una solicitud HTTP personalizada a
+        través de HTTPS.
+         options: Opciones de la solicitud, como la URL, los encabezados y el
+        método HTTP.
+         callback: Función de devolución de llamada que se ejecuta cuando se
+        completa la respuesta. Recibe el objeto de respuesta.
+        3. http.createServer(options, requestListener):Crea un servidor HTTPS.
+         options: Opciones para la creación del servidor, como el certificado y la
+        clave privada.
+         requestListener: Función que se ejecuta cuando se recibe una solicitud en
+        el servidor.
+        Estos son algunos de los métodos más utilizados del módulo https. Sin embargo,
+        hay otros métodos disponibles para operaciones más avanzadas, como el manejo
+        de certificados SSL/TLS y la configuración de agentes HTTP personalizados.
+        Recuerda que los métodos de https siguen la misma estructura y conceptos
+        básicos que los del módulo http, pero se utilizan para realizar solicitudes y crear
+        servidores seguros utilizando el protocolo HTTPS.
+        Puedes consultar la documentación oficial de Node.js para obtener más detalles
+        sobre cada método y su uso específico en el módulo https.
+
+        *!1.7.2 Que es NPM 
+        NPM (Node Package Manager) es el administrador de paquetes predeterminado
+        para Node.js, que es un entorno de tiempo de ejecución de JavaScript basado en
+        el motor V8 de Google Chrome. NPM permite a los desarrolladores de Node.js
+        compartir y reutilizar fácilmente el código JavaScript, así como gestionar las
+        dependencias de un proyecto.
+
+        Con NPM, puedes:
+
+        *?Instalar paquetes:
+        Puedes buscar, descargar e instalar paquetes de código
+        JavaScript disponibles públicamente en el registro de paquetes de NPM. Estos
+        paquetes pueden ser librerías, frameworks o cualquier otro tipo de código que
+        desees utilizar en tu proyecto de Node.js.
+
+        *?Gestionar dependencias
+        NPM te permite definir las dependencias de tu
+        proyecto en un archivo llamado "package.json". Este archivo contiene información
+        sobre las dependencias requeridas por tu proyecto, como el nombre del paquete,
+        la versión y las restricciones de versión. NPM puede resolver automáticamente las
+        dependencias y descargarlas en tu proyecto.
+
+        *?Actualizar paquetes:
+        Puedes utilizar NPM para actualizar fácilmente los paquetes
+        instalados en tu proyecto a versiones más recientes, lo que te permite beneficiarte
+        de las correcciones de errores y las nuevas funcionalidades.
+
+        *?Publicar tus propios paquetes:
+        
+
+
+
         
 
  */
